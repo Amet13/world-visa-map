47d71c3d9ee14ac2090dca787c05701aba831e35
Amet13 <admin@amet13.name>
2017-10-10 10:09:35 +0300

    move js and css files

--- js/getdata.js
+++ js/getdata.js
@@ -1,7 +1,9 @@
+var link = 'https://www.passportindex.org/comparebyPassport.php?p1=ru&fl=&s=yes'
 var page = require('webpage').create();
 var fs = require('fs');
 var path = 'output.txt';
-page.open('https://www.passportindex.org/comparebyPassport.php?p1=ru&fl=&s=yes', function () {
+
+page.open(link, function () {
 	fs.write(path, page.content);
 	phantom.exit();
 });
