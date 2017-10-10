cb7fe71ec27e465dc6b9569f8cec16b1003916cd
Amet13 <admin@amet13.name>
2017-10-10 15:40:54 +0300

    fix Crimea

--- generate.bash
+++ generate.bash
@@ -55,12 +55,12 @@ while read line; do
             STATE_NUM=4
             STATE="visa required"
             ;;
-	*)
-	    ;;
+        *)
+            ;;
     esac
 
     echo "		${line}" | sed -r "s/properties\":\{/properties\":\{\"status\":${STATE_NUM},\"data\":\"${STATE}\",/g" >> ${OUTPUT_FILE}
-done < <(grep "properties" ${INPUT_FILE} | grep -v "//")
+done < <(grep "properties" ${INPUT_FILE})
 
 echo "	]
 };" >> ${OUTPUT_FILE}
