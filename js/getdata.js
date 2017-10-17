67bb1d511774422ca726f5fd3e3e603e31586340
Amet13 <admin@amet13.name>
2017-10-17 14:24:07 +0300

    disable temporary file

--- js/getdata.js
+++ js/getdata.js
@@ -1,9 +1,7 @@
 var link = 'https://www.passportindex.org/comparebyPassport.php?p1=ru&fl=&s=yes'
 var page = require('webpage').create();
-var fs = require('fs');
-var path = '/tmp/output.txt';
 
 page.open(link, function () {
-	fs.write(path, page.content);
+	console.log(page.content);
 	phantom.exit();
 });
